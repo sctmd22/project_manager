b7ef707045e3f9669275a2dfd11896312a7e6b77
Marcel <marcel.dorma@scissorscreektesting.com>
2024-12-11 13:26:20 -0600

    Progress on measurements part of cyl form

--- GLOBALS.py
+++ GLOBALS.py
@@ -14,11 +14,14 @@ MOULD_TYPES = {
     "150x300_plastic":"150x300 Plastic",
 }
 
-LOAD_VOLUME_UNITS = [
-    "meters",
-    "yards"
-]
+LOAD_VOLUME_UNITS = {
+    "meters":"m",
+    "yards":"yd"
+}
 
+#Number of decimals to display when viewing a report
+    #As of 2024-12-10 it is stored in myssql as 3 digits
+LOAD_VOLUME_PRECISION = 1
 
 PROJECT_STATUS = {
     'active': "Active",
@@ -28,19 +31,36 @@ PROJECT_STATUS = {
 }
 
 
-''''
-#Define an enum for project status. 
-class PROJECT_STATUS(Enum):
-    ACTIVE = 'active'
-    COMPLETE = 'complete'
-    DELETED = 'deleted'
-    CANCELED = 'canceled'
 
+#Max characters for each VARCHAR type
+CYL_DATA_LIMITS = {
+    'created_by':50,
+    'report_title':255,
+    'ticket_num':50,
+    'project_name':50,
+    'supplier':50,
+    'load_num':50,
+    'truck_num':50,
+    'contractor':50,
+    'sampled_from':50,
+    'mix_id':50,
+    'po_num':50,
+    'placement_type':50,
+    'cement_type':50,
+    'load_volume':50
 
-PROJECT_STATUS_OPTIONS = {
-    PROJECT_STATUS.ACTIVE: "Active",
-    PROJECT_STATUS.COMPLETE: "Complete",
-    PROJECT_STATUS.DELETED: "Deleted",
-    PROJECT_STATUS.CANCELED: "Canceled"
 }
-'''
+
+
+CYL_CONDITIONS_TABLE = [
+    {'name':'Flow (mm)', 'property': 'flow', 'id':'cylConFlow', 'notesID':'cylConFlowNotes', 'SCC':True, 'CYL': False},
+    {'name':'T<sub>50</sub>(s)', 'property': 't_50', 'id':'cylConT50', 'notesID':'cylConT50Notes', 'SCC':True, 'CYL': False},
+    {'name':'VSI', 'property': 'vsi', 'id':'cylConVSI', 'notesID':'cylConVSINotes', 'SCC':True, 'CYL': False},
+    {'name':'Slump (mm)', 'property': 'slump', 'id':'cylConSlump', 'notesID':'cylConSlumpNotes', 'SCC':False, 'CYL':True},
+    {'name':'Air (%)', 'property':'air', 'id':'cylConAir', 'notesID':'cylConAirNotes', 'SCC':True, 'CYL':True},
+    {'name':'Unit Density (kg/m<sup>3</sup>)', 'property':'density', 'id':'cylConDensity', 'notesID':'cylConDensityNotes', 'SCC':True, 'CYL': True},
+    {'name':'Sample Temp (&deg;C)', 'property':'sampleTemp', 'id':'cylConSampleTemp', 'notesID':'cylConSampleTempNotes', 'SCC':True, 'CYL': True},
+    {'name':'Ambient Temp (&deg;C)', 'property':'ambientTemp', 'id':'cylConAmbientTemp', 'notesID':'cylConAmbientTempNotes', 'SCC':True, 'CYL': True},
+    {'name':'Initial Curing Conditions (&deg;C)', 'property':'initialTemp', 'id':'cylConInitialTemp', 'notesID':'cylConInitialTempNotes', 'SCC':True, 'CYL': True},
+]
+
